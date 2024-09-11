93abaa7ed5654cde94aba7b7541688b067744b45
haru-teck <ginchatoku1894@gmail.com>
2024-09-12 00:58:40 +0900

    画像保存できた

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,7 +1,5 @@
 class AnnouncementsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_announcement, only: [:show]
-
+  
   def index
     @announcements = Announcement.all
   end
@@ -9,25 +7,32 @@ class AnnouncementsController < ApplicationController
   def new
     @announcement = Announcement.new
   end
-
+  
+  
   def create
     @announcement = Announcement.new(announcement_params)
     if @announcement.save
-      redirect_to announcements_path, notice: 'お知らせが作成されました。'
+      redirect_to @announcement, notice: 'Announcement was successfully created.'
     else
       render :new
     end
   end
+  
+  def update
+    @announcement = Announcement.find(params[:id])
+    if @announcement.update(announcement_params)
+      redirect_to @announcement, notice: 'Announcement was successfully updated.'
+    else
+      render :edit
+    end
+  end
 
   def show
-  end
-
-  private
-
-  def set_announcement
     @announcement = Announcement.find(params[:id])
   end
-
+  
+  private
+  
   def announcement_params
     params.require(:announcement).permit(:title, :explanation, images: [])
   end
